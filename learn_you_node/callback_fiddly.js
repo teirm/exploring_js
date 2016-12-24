e08207ce9244a14a9b892d25b08f52ecb541ab18
teirm <tribunus.vii@gmail.com>
2016-12-24 18:50:52 -0500

    Learning callbacks

---
+++ learn_you_node/callback_fiddly.js
@@ -0,0 +1,17 @@
+var fs = require('fs'); // require is a special function provided by node
+var myNumber = undefined;  // we don't know what the number is yet since it is stored a file
+
+function addOne(callback) {
+    fs.readFile('number.txt', function doneReading(err, fileContents) {
+        myNumber = parseInt(fileContents);
+        myNumber++;
+        callback()
+    });
+}
+
+
+function logMyNumber() {
+    console.log(myNumber);
+}
+
+addOne(logMyNumber);
