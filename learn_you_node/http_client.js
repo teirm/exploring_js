ae7a30175169e5ebe14d92d299bb566ffa0fdb96
teirm <tribunus.vii@gmail.com>
2016-12-26 10:47:37 -0500

    Wrote http get client

--- learn_you_node/http_client.js
+++ learn_you_node/http_client.js
@@ -1,3 +1,25 @@
 var http = require('http');
 
+var url_to_get = process.argv[2];
 
+http.get(url_to_get, process_response);
+
+function process_response(response) {
+
+    response.setEncoding('utf8');
+
+    /* response.on('error', console.error); */
+
+    response.on('error', function (err) {
+        console.error(err);
+    });
+
+    /* A cleaner way to do this */
+    /* response.on('data', console.log); 
+
+    response.on('data', function (data) {
+        console.log(data);
+    });
+
+    //response.on('end', function (end) {
+}
