f60730f98f11e373c51f3ec98507aafd63e65728
teirm <tribunus.vii@gmail.com>
2017-01-02 10:30:01 -0500

    Wrote http server to upper case POSTs

---
+++ stream_adventure/http_server.js
@@ -0,0 +1,18 @@
+var http = require('http');
+var through = require('through2');
+
+var server = http.createServer(function (req, res) {
+    if (req.method == 'POST') {
+        req.pipe(through(write, end)).pipe(res);
+    } else {
+        res.end();
+    }
+});
+server.listen(process.argv[2]);
+
+function write (buf, _, next) {
+    this.push(buf.toString().toUpperCase());
+    next();
+}
+
+function end (done) { done(); }
