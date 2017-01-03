498f30415b7392d7ad2e9c92156ca11a77cbce68
teirm <tribunus.vii@gmail.com>
2017-01-03 10:59:08 -0500

    Copied combine solution -- Stream Adventures might be broken

--- stream_adventure/combiner.js
+++ stream_adventure/combiner.js
@@ -1,13 +1,49 @@
+/* NOT MY SOLUTION */
+/* USING TO LEARN MORE NODE */
+/* SOLUTION BY brunops: github.com/brunops */
+
 var combine = require('stream-combiner');
 var split = require('split');
 var through = require('through2');
+var zlib = require('zlib');
 
 module.exports = function () {
-    return combine(process.stdin,
-                   split(),
-                   through(function (line, _, next) {
-                        var parsed_line = JSON.parse(line);
-                   })
+
+    var genreInfo;
+
+    function write(buf) {
+        if (buf.length === 0) {
+            return;
+        }
+
+        buf = JSON.parse(buf);
+
+        if (buf.type === 'genre') {
+            if (genreInfo) {
+                /* Can queue chunks?? */
+                this.queue(JSON.stringify(genreInfo) + '\n');
+            } 
+            
+            /* Create an object */
+            genreInfo = {
+                name: buf.name,
+                books: []
+            }
+
+        } else {
+            genreInfo.books.push(buf.name);
+        }
+    }
+
+    function end() {
+        this.queue(JSON.stringify(genreInfo) + '\n');
+        this.queue(null);
+    };
+
+    return combine(split(),
+                   through(write, end),
+                   zlib.createGzip()
+                   );
 };
 
 
