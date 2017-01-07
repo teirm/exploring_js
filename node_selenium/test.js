fb8084aea191acf5ae4b79911f342d785fefdd03
teirm <tribunus.vii@gmail.com>
2017-01-07 14:17:57 -0500

    Fiddling with Mocha

---
+++ node_selenium/test.js
@@ -0,0 +1,35 @@
+/* A test using Mocha */
+
+var assert = require('assert');
+
+describe('Array', function() {
+    describe('#indexOf()', function() {
+        it('should return -1 when the value is not present', function() {
+            assert.equal(-1, [1,2,3].indexOf(4));
+        })
+    });
+});
+
+function add() {
+    return Array.prototype.slice.call(arguments).reduce(function(prev, curr) {
+        return prev + curr;
+    }, 0);
+}
+
+describe('add()', function() {
+    var tests = [
+        {args: [1,2],       expected: 3},
+        {args: [1,2,3],     expected: 6},
+        {args: [1,2,3,4],   expected: 10}
+    ];
+
+    tests.forEach(function(test) {
+        describe('#add()', function() {
+            it('correctly adds ' + test.args.length + 'args', function() {
+                var res = add.apply(null, test.args);
+                assert.equal(res, test.expected);
+            });
+        });
+    });
+});
+
